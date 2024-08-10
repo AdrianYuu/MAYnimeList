12fb81fa710f075ed11247d2e63b3ad5df732e4e
AdrianYuu <adrianyuu01@gmail.com>
2024-08-10 09:47:32 +0700

    feat: update flutter

--- fe_mal/lib/main.dart
+++ fe_mal/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: Scaffold(
+          body: Center(
+        child: ElevatedButton(onPressed: () {}, child: Text("Button")),
+      )),
+    );
+  }
 }
